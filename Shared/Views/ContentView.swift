12a4646dfaa8c86986760b7611a0ee89c1fbbb7c
Jack Elms <jackrobertelms@icloud.com>
2021-04-05 17:23:46 +0100

    Embedded contents in TabView

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -11,23 +11,37 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @FetchRequest(entity: Item.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Item.creationDate, ascending: false)]) var items: FetchedResults<Item>
+    @State private var showActionSheet = false
 
     var body: some View {
-        NavigationView {
-            List(items, id: \.self) { item in
-                NavigationLink(destination: InsightView(item: item)) { Text(item.creationDate, style: .date) }
-            }
-            .navigationBarTitle("Top Tree")
-            .toolbar {
-                ToolbarItemGroup(placement: .bottomBar) {
-                    Button("Insert") {
-                        try! Item.insertSamplesOneByOne(5)
+        VStack {
+            
+            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                NavigationView {
+                    List(items, id: \.self) { item in
+                        NavigationLink(destination: InsightView(item: item)) { Text(item.creationDate, style: .date) }
                     }
-                    Spacer()
-                    Button("Delete") {
-                        try! Item.deleteAllOneByOne()
-                    }
-                }
+                    .navigationBarTitle("Top Tree")
+                    .navigationBarItems(trailing:
+                        Button(action: {
+                            print("User icon pressed...")
+                        }) {
+                            Image(systemName: "person.crop.circle").imageScale(.large)
+                        }
+                    )
+                    .navigationBarItems(trailing:
+                        Button(action: {
+                            showActionSheet = true
+                        }) {
+                            Image(systemName: "ellipsis.circle.fill").imageScale(.large)
+                        }.actionSheet(isPresented: $showActionSheet,
+                                      content: {
+                                          ActionSheet(title: Text("ActionSheet"))
+                             })
+                    )
+                    
+                }.tabItem { Text("Top Tree") }.tag(1)
+                Text("Tab Content 2").tabItem { Text("Insights") }.tag(2)
             }
         }
     }
---
+++ Shared/Views/TodayView.swift
@@ -0,0 +1,20 @@
+//
+//  TodayView.swift
+//  Top Tree (iOS)
+//
+//  Created by Jack Elms on 05/04/2021.
+//
+
+import SwiftUI
+
+struct TodayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodayView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayView()
+    }
+}
